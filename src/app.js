371fed91c14cf86946f94f58c694a5150d4925f2
Maciej Loch <maqloch@gmail.com>
2019-08-16 17:42:14 +0200

    tutorial finished

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import { TweenMax, TimelineMax, Expo } from 'gsap/TweenMax';
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
+camera.position.z = 30;
 
 const renderer = new THREE.WebGLRenderer({
     antialias: true,
@@ -25,17 +25,33 @@ window.addEventListener('resize', () => {
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshLambertMaterial( { color: 0xffcc00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-// const ambient = new THREE.AmbientLight( 0x404040 );
-// scene.add(ambient)
+let objects = new Array(30).fill(null)
+objects.map(o => {
+    let cube = new THREE.Mesh( geometry, material );
+    cube.position.x = (Math.random() - .5) * 100
+    cube.position.y = (Math.random() - .5) * 100
+    scene.add( cube );
+    return cube
+})
 
 const light = new THREE.PointLight(0xFFFFFF, 1, 500)
-light.position.set(10, 0, 25)
+light.position.set(0, 0, 30)
 scene.add(light)
 
-const handleClick = () => {
+const handleClick = (e) => {
+    mouse.x = (e.clientX / window.innerWidth) * 2 - 1
+    mouse.y = - (e.clientY / window.innerHeight) * 2 + 1    
+    raycaster. setFromCamera(mouse, camera)
+
+    const intersects = raycaster.intersectObjects(scene.children, true)
+    intersects.map(element => {        
+        const tl = new TimelineMax().delay(.3)
+        tl.to(element.object.scale, 1, { x: 2, ease: Expo.easeOut })
+        tl.to(element.object.scale, .5, { x: .5, ease: Expo.easeOut })
+        tl.to(element.object.position, .5, { x: 0, ease: Expo.easeOut })
+        tl.to(element.object.rotation, .5, { y: Math.PI*.5, ease: Expo.easeOut }, '=-1.5')
+    })
     
 }
 
@@ -45,12 +61,6 @@ const animate = () => {
 }
 
 animate();
-
-const tl = new TimelineMax().delay(.3)
-tl.to(cube.scale, 1, { x: 2, ease: Expo.easeOut })
-tl.to(cube.scale, .5, { x: .5, ease: Expo.easeOut })
-tl.to(cube.position, .5, { x: 2, ease: Expo.easeOut })
-tl.to(cube.rotation, .5, { y: Math.PI*.5, ease: Expo.easeOut }, '=-1.5')
 
 document.body.addEventListener('click', handleClick)
 
